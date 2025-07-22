320af4653b23596f2e78da2439e786fcd90c5fae
akashBv6680 <153414012+akashBv6680@users.noreply.github.com>
2025-07-22 21:20:16 +0530

    Update Googlemeetai.py

--- Googlemeetai.py
+++ Googlemeetai.py
@@ -40,8 +40,7 @@ if st.button("🚀 Start Listening"):
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-notifications")
-
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager(driver_version="114.0.5735.90").install()),options=chrome_options)
     driver.get(meet_url)
 
     st.warning("⚠️ Login manually if prompted. Turn on captions in Meet.")
